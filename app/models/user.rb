b13b04880384c06a04ee8128d46db94d526b4be4
Vivi Poit <vwpoit@gmail.com>
2022-03-04 10:02:11 +0100

    add API key fields to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   devise :database_authenticatable, :rememberable
+
+  encrypts :api_key, :api_secret_key
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_25_134545) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_02_175417) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_25_134545) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "api_key"
+    t.string "api_secret_key"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
