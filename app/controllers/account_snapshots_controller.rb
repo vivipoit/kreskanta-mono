0f2f1d9045f3a3723a5c61a4c6a0a66c29d2e092
Vivi Poit <vwpoit@gmail.com>
2022-03-18 16:48:43 +0100

    Make account_snapshots#index handle user with no keys gracefully

--- app/controllers/account_snapshots_controller.rb
+++ app/controllers/account_snapshots_controller.rb
@@ -2,6 +2,10 @@
 
 class AccountSnapshotsController < ApplicationController
   def index
-    @account_snapshot = BinanceClient.account_snapshot(current_user)
+    if current_user.keys?
+      @account_snapshot = BinanceClient.account_snapshot(current_user)
+    else
+      redirect_to edit_user_path(current_user), alert: "Why you trying to get in places when you ain't got no keys?"
+    end
   end
 end
--- spec/features/user_sees_account_snapshot_spec.rb
+++ spec/features/user_sees_account_snapshot_spec.rb
@@ -3,39 +3,52 @@
 require 'rails_helper'
 
 describe 'user sees account snapshot' do
-  before do
-    user = User.create(
-      email: "#{SecureRandom.alphanumeric(4)}@email.com",
-      password: 'difficult-to-guess',
-      api_key: 'skeleton-key',
-      api_secret_key: 'hidden-key',
-      account_base: 'us'
-    )
+  context 'when there are keys' do
+    before do
+      user = User.create(
+        email: "#{SecureRandom.alphanumeric(4)}@email.com",
+        password: 'difficult-to-guess',
+        api_key: 'skeleton-key',
+        api_secret_key: 'hidden-key',
+        account_base: 'us'
+      )
 
-    sign_in user
-    visit account_snapshots_path
+      sign_in user
+      visit account_snapshots_path
+    end
+
+    it 'includes title' do
+      expect(page).to have_css('h2', text: 'Daily Account Snapshot')
+    end
+
+    it 'includes status' do
+      expect(page).to have_content('200')
+    end
+
+    it 'includes msg' do
+      expect(page).to have_content('something nice and sweet')
+    end
+
+    it 'includes snapshotVos' do
+      expect(page).to have_content('balances')
+      expect(page).to have_content('{:asset=>"USDT", :free=>"1.89109409", :locked=>"0.00000000"}')
+      expect(page).to have_content(':type=>"spot", :updateTime=>1576281599000')
+    end
   end
 
-  it 'includes title' do
-    expect(page).to have_css('h2', text: 'Daily Account Snapshot')
-  end
+  context 'when there are no keys' do
+    before do
+      user = User.create(
+        email: "#{SecureRandom.alphanumeric(4)}@email.com",
+        password: 'difficult-to-guess'
+      )
 
-  it 'includes description' do
-    expect(page).to have_css('p', text: 'described this stuff here')
-  end
+      sign_in user
+      visit account_snapshots_path
+    end
 
-  it 'includes status' do
-    expect(page).to have_content('200')
-  end
-
-  it 'includes msg' do
-    expect(page).to have_content('something nice and sweet')
-  end
-
-  it 'includes snapshotVos' do
-    expect(page).to have_content('balances')
-    expect(page).to have_content('{:asset=>"BTC", :free=>"0.09905021", :locked=>"0.00000000"}')
-    expect(page).to have_content('{:asset=>"USDT", :free=>"1.89109409", :locked=>"0.00000000"}')
-    expect(page).to have_content(':type=>"spot", :updateTime=>1576281599000')
+    it 'includes warning' do
+      expect(page).to have_content("Why you trying to get in places when you ain't got no keys?")
+    end
   end
 end
