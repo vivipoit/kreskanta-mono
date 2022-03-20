1177841fe2d3d2f8c566c1a201328d6521b4eda6
Vivi Poit <vwpoit@gmail.com>
2022-03-20 18:45:03 +0100

    fix RAILS_ENV issue for tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,7 @@ SimpleCov.start
 require 'webmock/rspec'
 
 require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
+ENV['RAILS_ENV'] = 'test'
 
 require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
