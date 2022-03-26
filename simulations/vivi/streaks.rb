2c8c94efedd8dc154edf2d9475a4ffe26b88640e
Vivi Poit <vwpoit@gmail.com>
2022-03-26 13:55:11 +0100

    add simulation code to project

---
+++ simulations/vivi/streaks.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+initial_balance = 5000
+
+usd_balance = initial_balance
+btc_balance = 0
+up_streak = 0
+down_streak = 0
+
+coin = Coin.find_by(own_symbol: 'BTC')
+asc_prices = Price.where(coin:).order(created_at: :asc)
+desc_prices = Price.where(coin:).order(created_at: :desc)
+
+{ # rubocop:disable Metrics/BlockLength
+  'asc_prices' => asc_prices,
+  'desc_prices' => desc_prices,
+  'asc_prices + desc_prices' => asc_prices + desc_prices,
+  'desc_prices + asc_prices' => desc_prices + asc_prices
+}.each do |name, prices|
+  prices.each_with_index do |price, index|
+    previous_price = (prices[index - 1].presence || Price.new).price
+
+    if price.price > previous_price
+      up_streak += 1
+      down_streak = 0
+
+      if up_streak > 5 && usd_balance > 100
+        usd_balance -= 100
+
+        btcs_bought = 100 / price.price
+        btc_balance += btcs_bought
+      end
+    end
+
+    next unless price.price < previous_price
+
+    down_streak += 1
+    up_streak = 0
+
+    next unless down_streak > 5 && btc_balance.positive?
+
+    btcs_sold = btc_balance / 4
+    btc_balance -= btcs_sold
+
+    usd_received = btcs_sold * price.price
+    usd_balance += usd_received
+  end
+
+  first_price = prices.first.price.round(6)
+  last_price = prices.last.price.round(6)
+  price_change = (last_price - first_price).round(6)
+  change_percentage = ((price_change / first_price) * 100.0).round(6)
+
+  btc_balance_in_usd = (btc_balance * last_price).round(6)
+
+  total = (usd_balance + btc_balance_in_usd).round(6)
+  change = (total - initial_balance).round(6)
+  percentage = ((change / initial_balance) * 100.0).round(6)
+
+  p "OUTCOME FOR #{name}"
+  p "usd_balance => #{usd_balance.round(6)}"
+  p "btc_balance => #{btc_balance.round(6)} (#{btc_balance_in_usd} USD)"
+  p "INITIAL NETWORTH: #{initial_balance} USD"
+  p "FINAL NETWORTH: #{total} USD"
+  p "GAIN/LOSS: #{change} USD (#{percentage} %)"
+  p "FIRST PRICE: #{first_price} USD"
+  p "LAST PRICE: #{last_price} USD"
+  p "PRICE CHANGE: #{price_change} USD (#{change_percentage} %)"
+  p "| streaks | #{name} | #{usd_balance.round(6)}"\
+    " | #{btc_balance.round(6)} <br/> (#{btc_balance_in_usd} USD)"\
+    " | #{initial_balance} | #{total} | #{change}"\
+    " | #{percentage} % | #{first_price} | #{last_price}"\
+    " | #{price_change} | #{change_percentage} |"
+end; nil
